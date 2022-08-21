66fdf6409e948dd22b717864b89ddc338509c71b
Fat Halimi <halimifat@gmail.com>
2022-08-22 00:18:12 +0200

    Commit Test

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -25,7 +25,7 @@ namespace Application.Activities
             }
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await context.Activities.FindAsync(request.Id);
+                return await context.Activities.FindAsync(request.Id);//
             }
         }
     }
